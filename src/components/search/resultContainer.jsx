9bab0b6eda2c8f5a79e88a334ba537476ce6a023
vcontrerasp13 <projectvcontreras@gmail.com>
2024-10-18 07:46:33 -0500

    inicio 18-10-24

--- src/components/search/resultContainer.jsx
+++ src/components/search/resultContainer.jsx
@@ -8,22 +8,20 @@ import Card from "@/app/search/ui/Card";
 import { productStore } from "@/store/productStore";
 import { userStore } from "@/store/userStore";
 import { ordernarEstablecimientosDistancia } from "../../../utils/helper";
-import box_empty from "/public/images/image1.svg"
-import { Loader } from "../ui/Loader";
+import box_empty from "/public/images/image1.svg";
 
 export const ResultContainer = ({ product, message, itemCode = "" }) => {
   const establecimientos = establecimientoStore((state) => state.establecimientos);
   const userdata = userStore((state) => state.user);
-  const [loading, setLoading] = useState(true);
-
-  console.log(product.length); //0
+  
+  const [loading, setLoading] = useState(true);  // Estado de carga
 
   const setProduct = productStore((state) => state.setProduct);
-  const productos = productStore((state) => state.product || []);
+  const productos = productStore((state) => state.product);
 
   useEffect(() => {
     setProduct(itemCode);
-  }, []);
+  }, [itemCode]);
 
   useEffect(() => {
     // Actualizar el estado de carga una vez que se procesen los productos
@@ -32,51 +30,45 @@ export const ResultContainer = ({ product, message, itemCode = "" }) => {
     }
   }, [productos, establecimientos]);
 
-  console.log(productos, '🚩🚩')
+  const productosConEstablecimiento = productos
+    .filter((producto) => establecimientos.some((e) => e.id === producto.codBode)) // Filtrar los productos con establecimiento válido
+    .map((producto) => {
+      const establecimiento = establecimientos.find((e) => e.id === producto.codBode);
+      return {
+        itemCode: producto.itemCode,
+        cantidad: producto.onHand,
+        id: establecimiento.id,
+        nombre: establecimiento.nombre,
+        lat: establecimiento.lat,
+        lon: establecimiento.lon,
+      };
+    });
 
-  if (productos.length > 0) {
+  const origen = establecimientos.find((e) => e.id === userdata.cod_establec);
+  const establec_ordenados = ordernarEstablecimientosDistancia(productosConEstablecimiento, origen);
 
-    const productosConEstablecimiento = productos
-      .filter((producto) => establecimientos.some((e) => e.id === producto.codBode)) // Filtrar los productos con establecimiento válido
-      .map((producto) => {
-        const establecimiento = establecimientos.find((e) => e.id === producto.codBode);
-        return {
-          itemCode: producto.itemCode,
-          cantidad: producto.onHand,
-          id: establecimiento.id,
-          nombre: establecimiento.nombre,
-          lat: establecimiento.lat,
-          lon: establecimiento.lon
-        };
-      });
-
-    // console.log(productosConEstablecimiento);
-
-
-    const origen = establecimientos.find((e) => e.id === userdata.cod_establec);
-    // const origen = establecimientos.find((e) => e.id == "ALM109");
-    const establec_ordenados = ordernarEstablecimientosDistancia(productosConEstablecimiento, origen);
-
-  } else {
-    setLoading(true)
-    return <span>Producto No encontrado en Ningún establecimiento</span>;
+  if (loading) {
+    // Mostrar indicador de carga mientras `loading` es true
+    return (
+      <div className="flex justify-center items-center h-64">
+        <p className="text-orange-400">Cargando establecimientos...</p>
+      </div>
+    );
   }
 
-
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
       {product.length > 0
-        ? (product.map((e, i) => <Card key={i} data={e} />))
+        ? product.map((e, i) => <Card key={i} data={e} />)
         : (
-          <div className="col-span-2 items-center ">
-            <p className=" text-center  text-orange-400 ">{message}</p>
+          <div className="col-span-2 items-center">
+            <p className="text-center text-orange-400">{message}</p>
             <Image
               src={box_empty}
               width={500}
               height={500}
-              alt="imag-search"
-              className=" h-62 mt-10 m-auto pointer-events-none opacity-30"
+              alt="imagen-vacía"
+              className="h-62 mt-10 m-auto pointer-events-none opacity-30"
             />
             <h2 className="text-2xl text-slate-500 mt-5">
               Lista de establecimientos con Stock
@@ -85,10 +77,9 @@ export const ResultContainer = ({ product, message, itemCode = "" }) => {
 
             {/* Mostrar lista de almacenes con stock */}
             <div className="flex flex-col gap-2 mt-5">
-              {loading ? <Loader />
-                : establec_ordenados.map((establecimiento, index) => (
-                  <EstablecimientoItem e={establecimiento} key={index} />
-                ))}
+              {establec_ordenados.map((establecimiento, index) => (
+                <EstablecimientoItem e={establecimiento} key={index} />
+              ))}
             </div>
           </div>
         )}
